5e032a95d95681de9cc7496a5b2245451c01b3cf
ramgpal <ramgpal912@gmail.com>
2023-07-31 16:59:37 +0530

    2ndCommit

---
+++ dictionary.py
@@ -0,0 +1,8 @@
+# in python dictionaries are collection of data, which is unordered, changeble & indexed
+# they have key & values
+thisdict = {
+"Name": "Ram",
+"Age": 19,
+"Occuption": "student"
+}
+print(thisdict)
