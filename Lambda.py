6275bd705ab80a8a3502515955b6a1e8fe2b341f
ramgpal <ramgpal912@gmail.com>
2023-07-31 17:28:31 +0530

    4thCommit

---
+++ Lambda.py
@@ -0,0 +1,6 @@
+# Lambda function is an anonymous funtion 
+
+#defining the funtion
+greet = lambda:print("Ram")
+#calling the function
+greet()
--- lamda.py
+++
@@ -1,5 +0,0 @@
-#  A lambda function, also known as an anonymous function, 
-# is a small, inline function in Python that doesn't have a name.
-square = lambda x: x**2
-print(square(2))
-  
