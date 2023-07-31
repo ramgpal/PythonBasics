6c47e61c9a6827bea62714f045073043cd630cb8
ramgpal <ramgpal912@gmail.com>
2023-07-31 17:30:27 +0530

    5thCommit

--- Lambda.py
+++ Lambda.py
@@ -1,6 +1,11 @@
 # Lambda function is an anonymous funtion 
 
 #defining the funtion
-greet = lambda:print("Ram")
+# greet = lambda:print("Ram")
 #calling the function
-greet()
+# greet()
+
+
+square = lambda x: x * x
+
+print(square(5))  # Output: 25
