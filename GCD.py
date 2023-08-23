e3005da252a3806858036e2e53c2748a589efd3b
ramgpal <ramgpal912@gmail.com>
2023-08-23 16:06:13 +0530

    2ndCommit

--- GCD.py
+++ GCD.py
@@ -1,10 +1,24 @@
+# Brute force approach
 m = int(input("Enter first number\n"))
 n = int(input("Enter 2nd number\n"))
-if m<n:
+# if m<n:
+#     min = m
+# else:
+#     min = n
+# for i in range (1, min+1):
+#     if m%i==0 and n%i==0:
+#         hcf = i
+# print(hcf)
+
+# Optimize approach -> Euclidean Algorithm
+if m < n:
+    max = n
     min = m
 else:
+    max = m
     min = n
-for i in range (1, min+1):
-    if m%i==0 and n%i==0:
-        hcf = i
-print(hcf)
+while(min != 0):
+    temp = min
+    min = max % min
+    max = temp
+print(max)
